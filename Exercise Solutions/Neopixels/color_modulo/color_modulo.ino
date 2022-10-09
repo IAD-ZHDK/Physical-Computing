d4dd61be9aa4b68809f8e5bc72ce5b76e020732b
Paulina <hi@zybinska.io>
2022-10-09 19:36:58 +0200

    HS22

--- Exercise Solutions/Neopixels/color_modulo/color_modulo.ino
+++ Exercise Solutions/Neopixels/color_modulo/color_modulo.ino
@@ -8,7 +8,7 @@
 #endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        8 
+#define PIN        2 
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS 8 
